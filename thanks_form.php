7488d9d86cfcff7434f84f4f86639bb1a1c59809
Your Name <your_email@example.com>
2025-02-03 16:14:30 +0300

    Improve contact form functionality and styling

--- thanks_form.php
+++ thanks_form.php
@@ -1,14 +1,26 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
     $email = $_POST["email"];
-	$to = "support@" . $_SERVER['HTTP_HOST'];
-    $subject = "New Contact Form Submission";
-    $body = "Name: $name\n";
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $to = "support@" . $_SERVER['HTTP_HOST'];
+    $emailSubject = "New message from contact form";
+    
+    $body = "First Name: $firstName\n";
+    $body .= "Last Name: $lastName\n";
     $body .= "Email: $email\n";
-    $body .= "Tel:\n$tel";
-    $headers = "From: $to \r\n";
-    mail($to, $subject, $body, $headers);
+    $body .= "Subject: $subject\n";
+    $body .= "Message:\n$message\n";
+
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+
+    mail($to, $emailSubject, $body, $headers);
+    
     // Redirect to thank you page
     header("Location: thanks.html");
     exit();
